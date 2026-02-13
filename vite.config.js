454917c8418f62ef1cb5404904c655346b35d691
24bai70398-Adityaj02 <24bai70398@cuchd.in>
2026-02-13 21:38:32 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,12 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-import tailwindcss from "@tailwindcss/vite";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-});
+  
+  preview: {
+    port: 4173,      // default preview port
+    open: true       // automatically open browser
+  }
+})
